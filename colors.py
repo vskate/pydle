658ced87fa30f0180e87deeb4c75e4ea8de369cd
vskate <wiktorkonk@gmail.com>
2024-01-13 01:52:00 +0100

    Added Utility Files

---
+++ colors.py
@@ -0,0 +1,13 @@
+# colors controlled by ANSI escape codes
+
+def green():
+    print("\033[32m", end="")
+
+def yellow():
+    print("\033[93m", end="")
+
+def clear_screen():
+    print("\033[H\033[J", end="")
+
+def reset():
+    print("\033[0m", end="")
